187006be42dfc43226181e30f6a74a2b3f032d92
4change <e_mail4gxf@163.com>
2022-09-13 09:48:45 +0800

    clear_garbage.sh: 清空回收站

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # shell-script-test
 shell脚本测试
+
+|  代码   | 说明  |
+|  ----  | ----  |
+| clear_garbage.sh  | Debian/Ubuntu 系统垃圾清理 |
+| memory_warning_mail.sh  | Debian/Ubuntu 系统内存告警邮件 |
--- clear_garbage.sh
+++ clear_garbage.sh
@@ -6,6 +6,7 @@ apt-get autoremove
 echo '---------------------------------------- 系统软件缓存清理完毕...'
 
 # 查找 ~/.cache 目录中日期大于365天的文件，直接删除之
+find /home/root/.cache/ -type f -atime +365 -delete
 find /home/fly/.cache/ -type f -atime +365 -delete
 echo '---------------------------------------- ~/.cache 目录清理完毕...'
 
@@ -21,3 +22,8 @@ echo '---------------------------------------- 系统日志清理完毕...'
 # 清除 deb 包卸载的残留文件
 dpkg -l |grep ^rc|awk '{print $2}' |sudo xargs dpkg -P 
 echo '---------------------------------------- deb 包卸载残留文件清理完毕...'
+
+# 清空回收站
+cd /root/.local/share/Trash && sudo rm -rf *
+cd /home/fly/.local/share/Trash && sudo rm -rf *
+echo '---------------------------------------- 回收站清理完毕...'
